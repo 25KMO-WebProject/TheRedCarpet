46005e9a491c046343b0d98ae856dc23ef813ff5
Riku <rikuangesleva@hotmail.com>
2026-09-10 19:53:49 +0300

    App.jsx cleaned and renamed TMDB_ACCESS_TOKEN

--- api/routes/searchRouter.js
+++ api/routes/searchRouter.js
@@ -47,7 +47,7 @@ router.get('/search', async (req, res) => {
 
     const response = await fetch(url, {
       headers: {
-        Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
         accept: 'application/json'
       }
     })
